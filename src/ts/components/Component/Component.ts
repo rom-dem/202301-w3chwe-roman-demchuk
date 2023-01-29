44991dc3c240ce131e2e2345a521d01343421197
Róman Demchuk <roman.demchuk@gmail.com>
2023-01-29 11:45:20 +0100

    Add header component js and css

---
+++ src/ts/components/Component/Component.ts
@@ -0,0 +1,20 @@
+import type { ComponentStructure } from "../../types/types";
+
+class Component implements ComponentStructure {
+  element: Element;
+
+  constructor(
+    private readonly parentElement: Element,
+    className: string,
+    tag = "div"
+  ) {
+    this.element = document.createElement(tag);
+    this.element.className = className;
+  }
+
+  render(): void {
+    this.parentElement.appendChild(this.element);
+  }
+}
+
+export default Component;
