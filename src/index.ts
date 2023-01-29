8adc4aef2c80178c37ef92758222f405b499f21d
Róman Demchuk <roman.demchuk@gmail.com>
2023-01-29 15:33:06 +0100

    Add iteration to gotPokeapi function

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import Component from "./ts/components/Component/Component.js";
 import HeaderComponent from "./ts/components/HeaderComponent/HeaderComponent.js";
 import { getPokeapi } from "./ts/getPokeapi/getPokeapi.js";
 
-const getPikachu = await getPokeapi("86");
-
 const container = document.querySelector(".root");
 
 const header = new HeaderComponent(container, "header", "header");
@@ -13,8 +11,3 @@ header.render();
 const article = new Component(container, "card", "article");
 
 article.render();
-
-const articleContent = document.querySelector(".card");
-
-articleContent.innerHTML =
-  getPikachu.sprites.other["official-artwork"].front_default;
--- src/ts/getPokeapi/getPokeapi.ts
+++ src/ts/getPokeapi/getPokeapi.ts
@@ -3,9 +3,19 @@ import type { PokemonData } from "../types/types.js";
 // Let pokemonData: PokemonData;
 const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 
-export const getPokeapi = async (id: string) => {
-  const response = await fetch(`${apiUrl}${id}`);
+export const getPokeapi = async (id: number): Promise<PokemonData[]> => {
+  const pokemons = [];
+  for (let position = 1; position <= id; position++) {
+    const response = fetch(`${apiUrl}${position}`);
+    pokemons.push(response);
+  }
 
-  const pokemonData = (await response.json()) as PokemonData;
+  const unresolvedPokemons = await Promise.all(pokemons);
+  const resolvedPokemons = unresolvedPokemons.map(async (unresolvedPokemon) =>
+    unresolvedPokemon.json()
+  );
+
+  const pokemonData = (await Promise.all(resolvedPokemons)) as PokemonData[];
+
   return pokemonData;
 };
